862bf5a791025f4710a576b08fe1bb830d17e26a
Carlos Cruz Martínez <56021993+CarlosXCruz@users.noreply.github.com>
2020-05-15 13:14:32 +0200

    Task-005: Done

--- complain/index.php
+++ complain/index.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="" />
+        <script src="" type="text/javascript"></script>
+        <title>Complain - dlgaApp</title>
+    </head>
 
+    <?php
+        require_once("../entity/member.php");
+
+        session_start();
+
+        if(!isset($_SESSION["member"])) {
+            header("Location: index.php");
+            exit;
+        } 
+    ?>
+
+    <body>
+        <header>
+            Gestión de Incidencias de la Delegación de la Escuela Técnica Superior de Ingeniería Informática
+        </header>
+        <main>
+            <div>
+                <a href="all/index.php">Ver incidencias</a>
+            </div>
+            <div>
+                <a href="new/index.php">Crear incidencia</a>
+            </div>
+            <div>
+                <a href="../profile.php">Volver</a>
+            </div>
+        </main>
+    </body>
+</html>
